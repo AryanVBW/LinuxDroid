47d8d2d1ed038cd5dfe010c5214f09301dbc149a
ARYAN <92390419+AryanVBW@users.noreply.github.com>
2023-03-18 22:41:20 +0530

    Update vivek.sh

--- vivek.sh
+++ vivek.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 PS3="Welcome Vivek : "
 
-select lng in Kali KALI_Root Termux Ubantu
+select lng in Termux Ubantu
 do
     case $lng in
-        "Kali")
-            nh;;
-        "KALI_Root")
-           nh -r;;
+        
         "Termux")
            exit  ;;
         "Ubantu")
-            bash ./start-ubuntu.sh;;
+            bash ./start-ubuntu20.sh;;
         *)
            echo "Ooops";;
     esac
