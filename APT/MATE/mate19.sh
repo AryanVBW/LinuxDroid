ca4c5f338ec8d9b8ffd74681a9bb4840b09944c4
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2023-10-29 22:25:13 +0000

    Add files via upload

---
+++ APT/MATE/mate19.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#Get the necessary components
+sudo apt-get update
+sudo apt install udisks2 -y
+echo "" > /var/lib/dpkg/info/udisks2.postinst
+sudo dpkg --configure -a
+sudo apt-mark hold udisks2
+sudo apt-get install mate-desktop-environment mate-terminal tigervnc-standalone-server tigervnc-common -y
+sudo apt purge fprintd libfprint0 libpam-fprintd -y
+sudo apt-get install xfe -y
+sudo apt purge fprintd
+sudo apt-get clean
+
+mkdir ~/.vnc
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+mate-session &" > ~/.vnc/xstartup
+
+echo " "
+
+echo "Running browser patch"
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
+./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
+
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo " "
+echo "Connect to this address will open a window with Mate Desktop Environment"
+echo " "
+echo " "
+echo " "
+echo "Running vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "To Kill VNC Server just run vncserver-stop"
+echo " "
+echo " "
+echo " "
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+vncpasswd
+vncserver-start
