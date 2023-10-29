0412be83f777991004655cdeff732126f6894f25
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2023-10-29 22:27:25 +0000

    Add files via upload

---
+++ Installer/Arch/amd64/additional.sh
@@ -0,0 +1,28 @@
+systemctl disable systemd-resolved.service
+rm /etc/resolv.conf
+mv resolv.conf /etc
+pacman-key --init
+echo "disable-scdaemon" > /etc/pacman.d/gnupg/gpg-agent.conf
+pacman-key --populate archlinux
+
+echo ""
+echo ""
+echo "Changing some permissions, please be patient"
+echo ""
+echo ""
+chmod 755 -R /bin /home /mnt /run /srv /tmp /var /boot /etc /opt /root /sbin /sys /usr
+echo "IMPORTANT"
+echo ""
+echo ""
+echo "Removing some unused packages, reclaims about 650MB"
+yes | LC_ALL=C.UTF-8 pacman -Rncs linux-firmware
+echo "If you are using Android 9 and above, you will encounter this error:"
+echo ""
+echo "could not change the root directory (Function not implemented)"
+echo ""
+echo "Simply ignore it as it does not do anything harmful"
+echo ""
+echo "updating Arch packages"
+yes | LC_ALL=C.UTF-8 pacman -Suuyy 
+echo ""
+echo ""
