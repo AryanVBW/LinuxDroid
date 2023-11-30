a246491832565a8af7f08c06fba549bd6b0b8ede
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2023-11-30 22:08:14 +0530

    Update parrot.sh

--- Scripts/InstallScript/Cli/parrot.sh
+++ Scripts/InstallScript/Cli/parrot.sh
@@ -22,7 +22,7 @@ if [ -f "parrot-rootfs.tar.xz" ];then
 rm -rf parrot-rootfs.tar.xz
 fi
 if [ ! -f "parrot-rootfs.tar.xz" ];then
-printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Downloading the parrot OS, please wait...\n"
+printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[LinuxDroid]:\e[0m \x1b[38;5;87m Downloading the parrot OS, please wait...\n"
 ARCHITECTURE=$(dpkg --print-architecture)
 case "$ARCHITECTURE" in
 aarch64) ARCHITECTURE=arm64;;
@@ -38,7 +38,7 @@ exit 1
 esac
 
 wget https://github.com/AryanVBW/Roofs/releases/download/${ARCHITECTURE}/parrot-rootfs-${ARCHITECTURE}.tar.xz?raw=true -q -O parrot-rootfs.tar.xz 
-printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Download complete!\n"
+printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[LinuxDroid]:\e[0m \x1b[38;5;87m Download complete!\n"
 
 fi
 	cur=`pwd`
